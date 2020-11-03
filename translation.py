e1e66baf16df27c1c789001e91110c988cc26037
appusejithin33 <68550804+appusejithin33@users.noreply.github.com>
2020-11-03 17:38:13 +0400

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,6 +1,6 @@
 class Translation(object):
     START_TEXT = """Hello,
-This is a Telegram URL Upload Bot made by @prgofficial
+This is a C GTelegram URL Upload Bot pro made by @Princedavitt
 
 <b>Please send me any Direct download URL link, I can upload to telegram as File/Video</b>
 
